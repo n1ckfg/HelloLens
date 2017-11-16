3e3e2b864afc60f552452da510b484f071dc1246
Nick Fox-Gieg <nick@fox-gieg.com>
2017-11-16 00:05:23 -0800

    new examples

---
+++ drawing_example/Assets/Scripts/LightningArtist_Custom/ChaseHandTarget.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChaseHandTarget : MonoBehaviour {
+
+    public float speed = 0.5f;
+
+    private Transform target;
+
+    void Update () {
+		if (target == null) {
+            target = GameObject.FindWithTag("HandTarget").transform;
+            if (target == null) return;
+        }
+
+        transform.position = Vector3.Lerp(transform.position, target.position, speed);
+	}
+}
