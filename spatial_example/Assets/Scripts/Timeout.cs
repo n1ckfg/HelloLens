3341e3fa7d0090770cfb4dc7513dfacac3857178
Nick Fox-Gieg <nick@fox-gieg.com>
2017-12-05 15:51:34 -0500

    spatial example

---
+++ spatial_example/Assets/Scripts/Timeout.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Timeout : MonoBehaviour {
+
+    public float delay = 5f;
+
+    IEnumerator Start() {
+        yield return new WaitForSeconds(delay);
+        Destroy(gameObject);
+    }
+
+}
