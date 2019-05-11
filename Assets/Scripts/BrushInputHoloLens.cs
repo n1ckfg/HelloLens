22c0a341fc3a69f80cebe6d076f206f03d45b9f3
n1ckfg <nick@fox-gieg.com>
2019-05-11 11:45:51 -0400

    reorganize

---
+++ Assets/Scripts/BrushInputHoloLens.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BrushInputHoloLens : MonoBehaviour {
+
+    public LightningArtist latk;
+    public HoloLensHandTracking.HandsTrackingController hands;
+    public int startingFrames = 12;
+
+    private bool firstRun = true;
+
+    private void Awake() {
+        if (latk == null) latk = GetComponent<LightningArtist>();
+    }
+
+    private void Update() {
+        if (firstRun) {
+            if (startingFrames > 0) {
+                for (int i = 0; i < startingFrames - 1; i++) {
+                    latk.inputNewFrame();
+                }
+                latk.inputPlay();
+            }
+            firstRun = false;
+        }
+
+        latk.clicked = hands.handPressed;
+    }
+
+}
